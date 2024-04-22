38ad2cdaff5f5edab17f46f920a09f09bccd43a2
sierrarajabali <sierra.rajabali26@ycdsbk12.ca>
2024-04-21 21:17:49 -0400

    done

--- Sketch.java
+++ Sketch.java
@@ -1,8 +1,22 @@
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class Sketch extends PApplet {
+  PImage imgBackground;
+  PImage imgLightingBolt;
+
+  float fltLightingX = 200;
+  float fltLightingY = 200;
+  float fltLightingSpeed = 2; 
+  float fltLightingDirectionX = -1; 
+  float fltLightingDirectionY = -1; 
 	
-	
+  float fltSqrX = width/2;
+  float fltSqrY = width/2; 
+  float fltSqrSpeed = 5; 
+  float fltSqrDirectionX = -1; 
+  float fltSqrDirectionY = -1; 
+
   /**
    * Called once at the beginning of execution, put your size all in this method
    */
@@ -16,21 +30,39 @@ public class Sketch extends PApplet {
    * values here i.e background, stroke, fill etc.
    */
   public void setup() {
-    background(210, 255, 173);
+   imgBackground = loadImage("background.jpeg");
+   imgBackground.resize(imgBackground.width*3, imgBackground.height*3);
+   imgLightingBolt = loadImage("lightingbolt.png");
+   imgLightingBolt.resize(imgLightingBolt.width/2, imgLightingBolt.height/2);
   }
 
   /**
    * Called repeatedly, anything drawn to the screen goes here
    */
   public void draw() {
-	  
-	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
+    image (imgBackground, 0, 0);
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+    image(imgLightingBolt, fltLightingX, fltLightingY); 
+    fltLightingX = fltLightingX + (fltLightingDirectionX*fltLightingSpeed);
+    if (fltLightingX < 0 || fltLightingX > width - imgLightingBolt.width){
+      fltLightingDirectionX = fltLightingDirectionX * -1; 
+    }
+    fltLightingY = (float)0.02 * (float)Math.pow(fltLightingX - 125, 2); 
+    fltLightingY = fltLightingY + (fltLightingDirectionY*fltLightingSpeed);
+    if (fltLightingY < 0 || fltLightingY > height - imgLightingBolt.height){
+      fltLightingDirectionY = fltLightingDirectionY * -1; 
+    }
+
+    fill(255,0,0); 
+    rect(fltSqrX, fltSqrY, 50, 50);
+      fltSqrX = fltSqrX + (fltSqrDirectionX*fltSqrSpeed);
+    if (fltSqrX < 0 || fltSqrX > width - 50){
+      fltSqrDirectionX *= -1; 
+    }
+
+    fltSqrY = fltSqrY + (fltSqrDirectionY*fltSqrSpeed);
+    if (fltSqrY < 0 || fltSqrY > height - 50){
+      fltSqrDirectionY *= -1; 
+    }
   }
-  
-  // define other methods down here.
 }
